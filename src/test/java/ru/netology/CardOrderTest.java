f9bf1f4f678c9c45c35aa861967a8684f9b36e03
Veronazavr <verochka10@list.ru>
2020-12-14 20:33:53 +0300

    added updated

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,4 +1,22 @@
 package ru.netology;
 
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class CardOrderTest {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action='/']");
+        $("[data-test-id=name] input").setValue("Вероника");
+        $("[data-test-id=phone] input").setValue("+79009990099");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+
+
+    }
 }
